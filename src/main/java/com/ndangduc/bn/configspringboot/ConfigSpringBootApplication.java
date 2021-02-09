1f6276864ba57109cc80c629672995e493f4e776
Duc Nguyen Dang <ducnd@mpos.vn>
2021-02-09 09:40:43 +0700

    Commit Lần 2

---
+++ src/main/java/com/ndangduc/bn/configspringboot/ConfigSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn.configspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndangduc/bn/configspringboot/ConfigSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndangduc.bn.configspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
